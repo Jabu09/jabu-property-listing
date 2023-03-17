4dfc6062fa71e5692a0c4cd6029d536363f4860c
Jabulani Madzivadondo <jabulani@basalt.co>
2023-03-17 18:32:49 +0200

    ffggfg

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,7 +91,7 @@ resource "aws_security_group_rule" "allow_inbound" {
 }
 
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "172.31.0.0/16"
   
   tags = {
     Name = local.app_name
@@ -99,7 +99,7 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "this" {
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "172.31.1.0/24"
   vpc_id     = "vpc-0ed56eafa9fc8c75e"
 
   tags = {
