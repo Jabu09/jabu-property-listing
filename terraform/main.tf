76e4028269e0b3385377e8a6636674aba8f5bd7e
Jabulani Madzivadondo <jabulani@basalt.co>
2023-03-17 17:13:30 +0200

    fix: neww

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,51 +2,121 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_ecr_repository" "nestjs_app" {
-  name = "nestjs-app"
+locals {
+  app_name = "nestjs-docker-app"
 }
 
-module "fargate" {
-  source  = "terraform-aws-modules/ecs/aws//examples/fargate"
-  version = "4.1.3"
-
-  name = "nestjs-app"
-
-  ecs_cluster_name = "nestjs-app-cluster"
-  task_definition = data.template_file.task_definition.rendered
-
-  container_definitions = jsonencode([{
-    name  = "nestjs-app"
-    image = "${aws_ecr_repository.nestjs_app.repository_url}:latest"
-    portMappings = [{
-      containerPort = 3000
-      hostPort      = 3000
-    }]
-  }])
-
-  # Configure the load balancer
-  lb_arn        = aws_lb.this.arn
-  lb_dns_name   = aws_lb.this.dns_name
-  lb_zone_id    = aws_lb.this.zone_id
-  lb_https      = false
+resource "aws_ecs_cluster" "this" {
+  name = local.app_name
 }
 
-resource "aws_lb" "this" {
-  name               = "nestjs-app-lb"
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = ["subnet-12345", "subnet-67890"] # Replace with your subnets
+resource "aws_ecs_task_definition" "this" {
+  family                = local.app_name
+  requires_compatibilities = ["FARGATE"]
+  network_mode          = "awsvpc"
+  cpu                   = "256"
+  memory                = "512"
+  execution_role_arn    = aws_iam_role.execution.arn
+  task_role_arn         = aws_iam_role.task.arn
+
+  container_definitions = jsonencode([
+    {
+      name = local.app_name
+      image = "jaybee095/jabu-property-listing:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_iam_role" "execution" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "task" {
+  name = "ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.execution.name
 }
 
 resource "aws_security_group" "this" {
-  name = "nestjs-app"
+  name = local.app_name
 }
 
-output "ecs_task_definition_arn" {
-  description = "ARN of the task definition"
-  value       = module.fargate.task_definition_arn
+resource "aws_security_group_rule" "allow_inbound" {
+  security_group_id = aws_security_group.this.id
+
+  type        = "ingress"
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
-data "template_file" "task_definition" {
-  template = file("${path.module}/task-definition.tpl")
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = local.app_name
+  }
+}
+
+resource "aws_subnet" "this" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.this.id
+
+  tags = {
+    Name = local.app_name
+  }
+}
+
+resource "aws_ecs_service" "this" {
+  name            = local.app_name
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  launch_type     = "FARGATE"
+
+  desired_count = 1
+
+  network_configuration {
+    subnets = [aws_subnet.this.id]
+
+    assign_public_ip = true
+    security_groups  = [aws_security_group.this.id]
+  }
 }
