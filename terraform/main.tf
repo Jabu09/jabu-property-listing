f99e4683a9b650165103ee27ba4c52d667ba0875
Jabulani Madzivadondo <jabulani@basalt.co>
2023-03-17 18:15:46 +0200

    dsd

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,124 +1,43 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-1"
 }
 
 locals {
-  app_name = "nestjs-docker-app"
+  app_name = "nestjs-app"
 }
 
-resource "aws_ecs_cluster" "this" {
+resource "aws_ecr_repository" "this" {
   name = local.app_name
 }
 
-resource "aws_ecs_task_definition" "this" {
-  family                = local.app_name
-  requires_compatibilities = ["FARGATE"]
-  network_mode          = "awsvpc"
-  cpu                   = "256"
-  memory                = "512"
-  execution_role_arn    = aws_iam_role.execution.arn
-  task_role_arn         = aws_iam_role.task.arn
+module "elastic_beanstalk_environment" {
+  source  = "cloudposse/elastic-beanstalk-environment/aws"
+  version = "0.34.0"
 
-  container_definitions = jsonencode([
-    {
-      name = local.app_name
-      image = "jaybee095/jabu-property-listing:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
-}
+  name = local.app_name
+  application = local.app_name
 
-resource "aws_iam_role" "execution" {
-  name = "ecs-task-execution-role-new-two"
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.8 running Docker"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+  instance_type = "t2.micro"
 
-resource "aws_iam_role" "task" {
-  name = "ecs-task-role-new-two"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.execution.name
-}
-
-resource "aws_security_group" "this" {
-  name = "nestjs-docker-app-new-three"
-  description = "Security group for NestJS Docker app"
-  vpc_id      = "vpc-0ed56eafa9fc8c75e"
-}
-
-resource "aws_security_group_rule" "allow_inbound" {
-  security_group_id = aws_security_group.this.id
-
-  type        = "ingress"
-  from_port   = 3000
-  to_port     = 3000
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
-  
-  tags = {
-    Name = local.app_name
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "vpc-0ed56eafa9fc8c75e"
   }
-}
 
-resource "aws_subnet" "this" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id     = "vpc-0ed56eafa9fc8c75e"
-
-  tags = {
-    Name = local.app_name
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"
   }
-}
 
-resource "aws_ecs_service" "this" {
-  name            = local.app_name
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.this.arn
-  launch_type     = "FARGATE"
-
-  desired_count = 1
-
-  network_configuration {
-    subnets = [aws_subnet.this.id]
-
-    assign_public_ip = true
-    security_groups  = [aws_security_group.this.id]
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "DOCKER_REGISTRY"
+    value     = aws_ecr_repository.this.repository_url
   }
+
+  # Add more settings as needed, such as for RDS, environment variables, etc.
 }
